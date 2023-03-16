d487b229336afada3772d81dc4dae72fa111882c
carraes <carraeshb@gmail.com>
2023-03-16 09:43:41 -0300

    feat: controls state, creates UserData state, set components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+
+export interface UserData {
+  firstName: string
+  lastName: string
+  progress: number
+}
+
 function App() {
+  const [userData, setUserData] = useState<UserData>({
+    firstName: '',
+    lastName: '',
+    progress: 0,
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setUserData((prev) => ({ ...prev, [name]: value }))
+  }
+
   return (
-    <main>
-      <h1>Hello</h1>
-    </main>
+    <>
+      <Header userData={userData} handleChange={handleChange} />
+      <Main />
+    </>
   )
 }
 
