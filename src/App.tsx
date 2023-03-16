b587bfc804b1ea7c86274adfadba02c622727f9d
carraes <carraeshb@gmail.com>
2023-03-16 15:16:54 -0300

    refactor: put states on app, so wont need context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
+import { randomColor } from './utils/randomColor'
 
 export interface UserData {
   firstName: string
   lastName: string
   progress: number
+}
+
+export interface CleanUpData {
+  name: string
+  value: number
+  color: string
+  key: number
 }
 
 function App() {
@@ -14,6 +22,28 @@ function App() {
     lastName: '',
     progress: 0,
   })
+  const [data, setData] = useState<CleanUpData[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:8080/users')
+      const data = await response.json()
+      cleanUpData(data)
+    }
+
+    fetchData()
+  }, [])
+
+  const cleanUpData = (data: UserData[]) => {
+    const cleanedData: CleanUpData[] = data.map(({ firstName, lastName, progress }, index) => ({
+      name: `${firstName} ${lastName}`,
+      value: progress * 100,
+      color: `${randomColor()}`,
+      key: index,
+    }))
+
+    setData(cleanedData)
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
@@ -23,7 +53,7 @@ function App() {
   return (
     <>
       <Header userData={userData} handleChange={handleChange} />
-      <Main />
+      <Main data={data} />
     </>
   )
 }
