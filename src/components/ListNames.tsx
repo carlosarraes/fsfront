9b105231c2cbf9aee59fad63036c875846ad068c
carraes <carraeshb@gmail.com>
2023-03-16 15:17:25 -0300

    feat: component to list names with assigned color

---
+++ src/components/ListNames.tsx
@@ -0,0 +1,20 @@
+import { CleanUpData } from '../App'
+
+interface ListNamesProps {
+  data: CleanUpData[]
+}
+
+const ListNames = ({ data }: ListNamesProps) => {
+  return (
+    <ul className="flex flex-col gap-2 w-1/2">
+      {data.map(({ name, color, key }) => (
+        <div key={key} className="flex h-full gap-2">
+          <div className="w-6 h-6 self-center" style={{ backgroundColor: color }} />
+          <li className="self-center">{name}</li>
+        </div>
+      ))}
+    </ul>
+  )
+}
+
+export default ListNames
