8475eedc2918f5c667e50701fbb3be92482c195c
carraes <carraeshb@gmail.com>
2023-03-16 09:44:13 -0300

    feat: Header componet, stylized, setup HeaderProps, controlled inputs

---
+++ src/components/Header.tsx
@@ -0,0 +1,64 @@
+import { AiOutlinePercentage } from 'react-icons/ai'
+
+interface HeaderProps {
+  userData: {
+    firstName: string
+    lastName: string
+    progress: number
+  }
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+function Header({ userData, handleChange }: HeaderProps) {
+  const { firstName, lastName, progress } = userData
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('click')
+  }
+
+  return (
+    <header className="flex justify-center bg-cyan-500 py-10">
+      <form className="flex gap-6" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={handleChange}
+          className="p-2 rounded-sm"
+          placeholder="First name"
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={handleChange}
+          className="p-2 rounded-sm"
+          placeholder="Last name"
+        />
+        <div className="relative">
+          <input
+            type="number"
+            name="progress"
+            value={progress}
+            onChange={handleChange}
+            className="pr-10 p-2 rounded-sm text-center"
+            placeholder="Progress (Ex: 40)"
+          />
+          <span className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+            <AiOutlinePercentage className="h-5 w-5 text-black" />
+          </span>
+        </div>
+        <button
+          type="submit"
+          disabled
+          className="border border-white text-white px-12 rounded-md hover:cursor-pointer duration-150 hover:enabled:bg-white hover:enabled:text-cyan-500 disabled:cursor-not-allowed disabled:text-gray-500 disabled:border-gray-500"
+        >
+          SEND
+        </button>
+      </form>
+    </header>
+  )
+}
+
+export default Header
