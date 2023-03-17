3221d41836110dd33cb6653222ed041486b1f502
carraes <carraeshb@gmail.com>
2023-03-17 06:57:32 -0300

    feat: colored names/label

--- src/components/ListNames.tsx
+++ src/components/ListNames.tsx
@@ -9,8 +9,10 @@ const ListNames = ({ data }: ListNamesProps) => {
     <ul className="flex flex-col gap-2 w-1/2">
       {data.map(({ name, color, key }) => (
         <div key={key} className="flex h-full gap-2">
-          <div className="w-6 h-6 self-center" style={{ backgroundColor: color }} />
-          <li className="self-center">{name}</li>
+          <div className="w-6 h-6 self-center rounded-md" style={{ backgroundColor: color }} />
+          <li className="self-center" style={{ color: color }}>
+            {name}
+          </li>
         </div>
       ))}
     </ul>
