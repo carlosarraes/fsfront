6f026aabd1c1b77253163ac09cbdf2aa5ba5ed06
carraes <carraeshb@gmail.com>
2023-03-19 03:06:17 -0300

    chore: add coverage and msw

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,8 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/tests/setupTests.ts'],
+    coverage: {
+      provider: 'c8',
+    },
   },
 })
